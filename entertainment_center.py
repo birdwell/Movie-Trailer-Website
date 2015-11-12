576b3d38e199de84b794e9ca5006d346696d213d
Josh Birdwell <jbirdwell34@gmail.com>
2015-11-12 16:25:36 -0600

    add init files

---
+++ entertainment_center.py
@@ -0,0 +1,16 @@
+import media
+import fresh_tomatoes
+
+# Create movies list
+movies = []
+
+# Add movie to movies
+def add_movie(movie):
+	movies.append(movie)
+
+# Add movies
+add_movie(media.Movie('Hitch','https://www.youtube.com/watch?v=dMaq_pfxs-0','http://images.moviepostershop.com/hitch-movie-poster-2005-1020243939.jpg','2005'))
+add_movie(media.Movie('IRobot','https://www.youtube.com/watch?v=s0f3JeDVeEo','https://upload.wikimedia.org/wikipedia/en/3/3b/Movie_poster_i_robot.jpg','2004'))
+
+# Open movies page
+fresh_tomatoes.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,15 @@
+class Movie:
+	"""A structure for movies
+	
+	Attributes:
+		title: A string of the movie title
+		trailer_youtube_url: A string of the movie youtube trailer_url
+		poster_image_url: A string of the movie poster image
+	"""
+	
+	# Constructor for movie class 
+	def __init__(self, title, trailer_url, poser_url, year):
+		self.title = title
+		self.poster_image_url = poser_url
+		self.trailer_youtube_url = trailer_url
+		self.year = year
